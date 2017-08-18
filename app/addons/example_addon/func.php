3b811b696928a0f26c631352c8d416afe57e62a5
dhaupin <dhaupin@gmail.com>
2017-08-18 15:25:50 -0400

    Backticks removed

--- app/addons/example_addon/func.php
+++ app/addons/example_addon/func.php
@@ -50,8 +50,9 @@ function fn_example_addon_get_product_fields($fields) {
 
 // Example - DB install/uninstall routines
 function fn_example_addon_install() {
-	db_query("ALTER TABLE `?:products` ADD `example_addon_flag` char(1) NOT NULL");
+	db_query("ALTER TABLE ?:products ADD `example_addon_flag` char(1) NOT NULL");
 }
+
 function fn_example_addon_uninstall() {
 	if (Registry::get('addons.example_addon.example_del') == 'Y') {
 		db_query("ALTER TABLE ?:products DROP COLUMN `example_addon_flag`;");
